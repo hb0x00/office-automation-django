0cfaf0fcdd0970764a2ebfbb838e7624fd3a2e36
hb0x00 <hackersboom0x00@proton.me>
2024-02-14 09:52:36 +0530

    added base template added gitignore

--- office_automation/urls.py
+++ office_automation/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+def base(req):
+    return render(req, "base.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include("ProjectAuth.urls"))
+    path('auth/', include("ProjectAuth.urls")),
+    path("", base, name="")
 ]
